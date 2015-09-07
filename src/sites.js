cb074a40b9ab704254c5763420d27bdef5dd7654
Fadee Kannah <fadeekannah@gmail.com>
2015-09-07 09:40:51 -0700

    added support for https

--- src/sites.js
+++ src/sites.js
@@ -1,18 +1,18 @@
 function getPrices(){
   //returns the elements that have the prices on them
-  if(window.location.href.indexOf('http://myfigurecollection.net/') > -1) {
+  if(window.location.href.indexOf('myfigurecollection.net/') > -1) {
      return jQuery('.sd:first li label:contains("Price")').closest('li').children( 'div' );
   }
-  else if(window.location.href.indexOf('http://www.1999.co.jp/eng') > -1) {
+  else if(window.location.href.indexOf('www.1999.co.jp/eng') > -1) {
     return jQuery('[id^=masterBody_ProductList_lvProductListTop_lblPrice]');
   }
-  else if(window.location.href.indexOf('http://jungle-scs.co.jp/') > -1 || window.location.href.indexOf('http://goodsmile-global.ecq.sc/') > -1) {
+  else if(window.location.href.indexOf('jungle-scs.co.jp/') > -1 || window.location.href.indexOf('goodsmile-global.ecq.sc/') > -1) {
     return jQuery('.price');
   }
-  else if(window.location.href.indexOf('http://biginjap.com/') > -1 ) {
+  else if(window.location.href.indexOf('biginjap.com/') > -1 ) {
     return jQuery('.price, #our_price_display');
   }
-  else if(window.location.href.indexOf('http://order.mandarake.co.jp/') > -1 ) {
+  else if(window.location.href.indexOf('order.mandarake.co.jp/') > -1 ) {
     return jQuery('div.basicinfo dl :nth-child(6) p, div.price > p');
   }
   else{
